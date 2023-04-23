4ccdebf611d8fdd5b7aa3dc72d5f5a4ae62f2aea
root <root@homeassistant.local.hass.io>
2023-04-24 00:00:00 +0100

    Commit on 2023-04-24 00:00

--- my_external_components/test/ng_v0.1/ow_bus_ng/ow_bus_component.h
+++ my_external_components/test/ng_v0.1/ow_bus_ng/ow_bus_component.h
@@ -49,22 +49,3 @@ class ESPHomeOneWireNGComponent : public Component {
 
 } // namespace ow_bus_ng
 } // namespace esphome
-
-// ESPHomeOneWireNGComponent::ESPHomeOneWireNGComponent(OneWireBusPinConfig pin_config, 
-//                                InternalGPIOPin *input_pin,
-//                                InternalGPIOPin *output_pin,
-//                                InternalGPIOPin *pin) {
-//   if (pin_config == ONEWIRE_BUS_PIN_SINGLE && pin != nullptr) {  
-//     this->split_io_ = ONEWIRE_BUS_PIN_SINGLE; 
-//   } 
-  
-//   else if (pin_config == ONEWIRE_BUS_PIN_SPLIT && input_pin != nullptr && output_pin != nullptr) {
-//     this->split_io_ = ONEWIRE_BUS_PIN_SPLIT;
-//     this->input_pin_ = input_pin;
-//     this->output_pin_ = output_pin;
-//   }
-  
-//   if (this->split_io_ == ONEWIRE_BUS_PIN_SINGLE) {
-//     this->pin_ = pin; 
-//   }
-// }
